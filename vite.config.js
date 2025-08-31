2392f2e4c707bfdf0b9c216258aee6a0311e6101
kumaranpachiyappan822 <kumaranpachiyappan822@gmail.com>
2025-08-31 19:54:09 +0530

    netlyfy hosting changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/genuine_hydraulics/",   // 👈 IMPORTANT for GitHub Pages
   plugins: [react()],
+  base: "./",   // 👈 Netlify / local relative paths
 })
 
 
