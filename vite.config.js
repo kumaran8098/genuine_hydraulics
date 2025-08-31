096f91502c982a61b151e0b8f868d95e5fca887e
kumaranpachiyappan822 <kumaranpachiyappan822@gmail.com>
2025-08-31 18:51:25 +0530

    Set base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/genunie_hydraulics/",   // 👈 IMPORTANT for GitHub Pages
+  base: "/geunie_hydraulics/",   // 👈 IMPORTANT for GitHub Pages
   plugins: [react()],
 })
 
